07f8b8445780ddfd54e282e61b961ff9447782e0
KSLeeDev <lllllks25@naver.com>
2023-05-25 15:47:41 +0000

    front-3rd-commit-ks

--- frontend/components/Signinbox.js
+++ frontend/components/Signinbox.js
@@ -1,32 +1,69 @@
+import { useState } from 'react';
 import styles from '../styles/Signin.module.css';
 import BootstrapHead from "./BootstrapHead";
 import Link from "next/link";
 
-function box() {
+function SignInForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <BootstrapHead/>
       {/* 아이콘 */}
-      <img className={styles.icon} src="../logo.png"></img>
+      <div className={styles.iconWrapper}>
+        <img src="/logo.png" alt="icon" className={styles.icon} />
+        <h1>Sign In</h1>
+      </div>
       {/* 전체 박스 */}
-      <div className={styles.signinbox}>
+      <form className={styles.signinbox} onSubmit={handleSignIn}>
         {/* 이름 입력받기 */}
         <div>Username</div>
-        <div><input className={styles.input} id="name"/></div>
+        <div>
+          <input
+            className={styles.input}
+            id="username"
+            value={username}
+            pattern="^[a-zA-Z0-9ㄱ-힣]+"
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
         {/* 줄 사이 공백 */}
         <div className={styles.space}></div>
         {/* 패스워드 입력받기 */}
         <div>Password</div>
-        <div><input className={styles.input}id="name"/></div>
+        <div>
+          <input
+            className={styles.input}
+            id="password"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
         {/* 줄 사이 공백 */}
         <div className={styles.space}></div>
         {/* 로그인하는 버튼 */}
-        <div className={styles.click}><div className={styles.mid}>sign in</div></div>
-      </div>
+        <button type="submit" className={styles.submitButton}>Sign In</button>
+      </form>
       {/* 계정 만들기 */}
       <div className={styles.account}>
-        New to ?&nbsp;
-        <Link href={'/'}>
+        New to GOSU?&nbsp;
+        <Link href="/">
           Create an account.
         </Link>
       </div>
@@ -34,4 +71,4 @@ function box() {
   );
 }
 
-export default box;
+export default SignInForm;
