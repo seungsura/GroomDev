1b97fce9664f0a43c1ed292e4c04cab57f3ca5a3
KSLeeDev <lllllks25@naver.com>
2023-05-26 12:55:18 +0000

    front-3rd-commit-ks

--- frontend/components/Signinbox.js
+++ frontend/components/Signinbox.js
@@ -3,7 +3,6 @@ import styles from '../styles/Signin.module.css';
 import BootstrapHead from "./BootstrapHead";
 import Link from "next/link";
 
-
 function SignInForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
